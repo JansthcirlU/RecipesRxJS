8b1153cd323e6d6566fbe515f126b11f4fd39bd3
Bryan <dekrobr@cronos.be>
2024-06-13 08:44:41 +0200

    Add recipe creation component

---
+++ recipes-rxjs/src/app/components/recipe-creation/recipe-creation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-creation',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-creation.component.html',
+  styleUrl: './recipe-creation.component.css'
+})
+export class RecipeCreationComponent {
+
+}
